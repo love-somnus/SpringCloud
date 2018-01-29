0be833135021552c0f737a249af1fd851002d0a8
love-somnus <928200207@qq.com>
2018-01-29 18:06:59 +0800

    SpringCloud整合

---
+++ SpringCloud-ribbon/src/main/java/com/somnus/springcloud/controller/ConsumerController.java
@@ -0,0 +1,20 @@
+package com.somnus.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=1&b=2", String.class).getBody();
+    }
+
+}
