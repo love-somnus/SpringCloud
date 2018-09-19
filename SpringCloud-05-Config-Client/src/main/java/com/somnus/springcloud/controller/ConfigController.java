88195f7716bb87f3df2885f64ca411c1e5a96de0
love-somnus <928200207@qq.com>
2018-09-19 09:51:30 +0800

    学无止境

---
+++ SpringCloud-05-Config-Client/src/main/java/com/somnus/springcloud/controller/ConfigController.java
@@ -0,0 +1,32 @@
+/**
+ * <p>Title: ConfigController.java </p>
+ * <p>Description: TODO(用一句话描述该文件做什么) </p>
+ *
+ * @author pc
+ * @version 1.0.0
+ * @date 2018年9月18日
+ */
+package com.somnus.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: ConfigController
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author pc
+ * @date 2018年9月18日
+ */
+@RestController
+public class ConfigController {
+	
+	@Value(value="${jdbc.password}")
+	private String password;
+	
+	@GetMapping("hi")
+	public String hi() {
+		return password;
+	}
+
+}
