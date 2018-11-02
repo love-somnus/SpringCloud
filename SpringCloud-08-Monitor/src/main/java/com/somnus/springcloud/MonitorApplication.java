b4bfae97fbb4b2ce18178397e869d954cb632229
love-somnus <928200207@qq.com>
2018-11-02 16:29:45 +0800

    love-somnus <928200207@qq.com>

--- SpringCloud-08-Monitor/src/main/java/com/somnus/springcloud/MonitorApplication.java
+++ SpringCloud-08-Monitor/src/main/java/com/somnus/springcloud/MonitorApplication.java
@@ -3,14 +3,16 @@ package com.somnus.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
-@EnableHystrix
+@EnableTurbine
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @SpringBootApplication
-public class RibbonApplication {
+public class MonitorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RibbonApplication.class, args);
+		SpringApplication.run(MonitorApplication.class, args);
 	}
 }
